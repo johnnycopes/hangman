6acf767ebb5e2ca81ee2f6831f7fc3f793298079
John Coppola <jdcoppola@gmail.com>
2016-09-22 20:33:43 -0400

    starts trying to make guessed letters visible

--- hangman.py
+++ hangman.py
@@ -6,12 +6,14 @@ Player 1 chooses a word and player 2 tries to guess what it is,
 one letter at a time, in seven guesses or less.
 """
 
-# Ask for word to be guessed (characters hidden on screen), then split
-# it to an array
+# Ask for word to be guessed (characters hidden on screen)
 target_word = getpass.getpass('Player 1, choose a word: ')
 
 # Set guess counter
 guesses_remaining = 7
+
+# Make visual map of word to guess
+word_to_guess = "*" * len(target_word)
 
 # Create an empty list to store the user's guesses
 guessed_letters = []
@@ -26,17 +28,20 @@ while guesses_remaining > 0:
     guesses_remaining -= 1
 
     # Show a visual map of the word
-    print "Word to guess: ", "* " * len(target_word)
+    print "Word to guess: %s" % word_to_guess
 
     # Prompt the user to guess another letter
     guess = raw_input("Guess a letter: ")
 
     if guess in target_word:
         print "\nNice!"
+        word_to_guess = list(word_to_guess)
+        print word_to_guess
+        # print target_word.find(guess)
     else:
         print "\nNope."
 
-    # Append guessed number to beginning of guessed_letters
+    # Append guessed number to beginning of guessed_letters list
     guessed_letters.insert(0, guess)
     # Show guessed letters after first guess
     if guesses_remaining < 7:
