de6a239f255eae66ddb064cadb376acb02e9ba4a
John Coppola <jdcoppola@gmail.com>
2016-09-25 21:57:12 -0400

    letters in hidden word are visible after correct guess

--- hangman.py
+++ hangman.py
@@ -8,36 +8,37 @@ one letter at a time, in seven guesses or less.
 
 # Ask for word to be guessed (characters hidden on screen)
 target_word = getpass.getpass('Player 1, choose a word: ')
-
-# Set guess counter
-guesses_remaining = 7
-
 # Make visual map of word to guess
 word_to_guess = "*" * len(target_word)
 
-# Create an empty list to store the user's guesses
+# Track guesses
+guesses_remaining = 7
 guessed_letters = []
 
 while guesses_remaining > 0:
 
-    # Show guesses remaining, then subtract one
     if guesses_remaining == 1:
         print "\n%d guess remaining." % guesses_remaining
     else:
         print "\n%d guesses remaining." % guesses_remaining
     guesses_remaining -= 1
-
-    # Show a visual map of the word
     print "Word to guess: %s" % word_to_guess
 
-    # Prompt the user to guess another letter
     guess = raw_input("Guess a letter: ")
 
     if guess in target_word:
+        # Find out where the guessed letter occurs in the target word
         print "\nNice!"
-        word_to_guess = list(word_to_guess)
+        indices = []
+        for i, j in enumerate(list(target_word)):
+            if j == guess:
+                indices.append(i)
+        # Show guessed letter in visual map
+        for index in indices:
+            word_to_guess = list(word_to_guess)
+            word_to_guess[index] = guess
+        word_to_guess = "".join(word_to_guess)
         print word_to_guess
-        # print target_word.find(guess)
     else:
         print "\nNope."
 
